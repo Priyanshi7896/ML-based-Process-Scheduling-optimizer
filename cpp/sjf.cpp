f325c9edb65f6c2e0e6d02d83ca73760a349398d
Priyanshi7896 <mpriyanshi72@gmail.com>
2025-05-21 13:15:39 +0530

    Modifying sjf

--- cpp/sjf.cpp
+++ cpp/sjf.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
-#include "json.hpp"  // Instead of #include <nlohmann/json.hpp>
+#include "json.hpp"  // Instead of #include <nlohmann/json.hpp>c
 
 using json = nlohmann::json;
 using namespace std;
